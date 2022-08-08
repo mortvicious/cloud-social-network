7f791a78f27bd8d14c2202a09ff28ba26043993b
mortvicious <shindo.shitai@gmail.com>
2022-08-08 19:26:15 +0400

    change tslint to eslint, refactor comment input in post

--- client/src/components/PostCommentInput.tsx
+++ client/src/components/PostCommentInput.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 
 const PostCommentInput = () => {
-    return (<Container>
-        <input className='input-primary' type="text" />
-    </Container>)
+	return (
+		<form className='d-flex gap-1 align-items-center mt-2'>
+			<input
+				className='bg-light border p-1 mt-1 w-100 post-comment-input'
+				placeholder='Share your thoughts'
+				type='text'
+			/>
+			<button className='btn btn-primary btn-sm'>
+				<i className='bi bi-send'></i>
+			</button>
+		</form>
+	)
 }
 
 export default PostCommentInput
