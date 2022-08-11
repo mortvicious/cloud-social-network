6d5985a17c9d03541327db4b4969d5ce5f3a0d1d
mortvicious <shindo.shitai@gmail.com>
2022-08-11 18:48:23 +0400

    + post input bound with mobx

--- client/src/components/Feed/FeedCreatePost.tsx
+++ client/src/components/Feed/FeedCreatePost.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react'
-
-const FeedCreatePost: FC = () => {
-  return (
+import newPostStore from '../../store/NewPost/NewPost'
+import {observer} from 'mobx-react-lite'
+const FeedCreatePost: FC = observer(() => {
+	const handleSendBtn = (e: React.MouseEvent<HTMLButtonElement>) => {}
+	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		newPostStore.setContent(e.currentTarget.value)		
+	}
+	return (
 		<div className='input-group'>
-			{/* <span className='input-group-text'>With textarea</span> */}
-			<textarea placeholder='Share your thoughts!' className='form-control' aria-label='With textarea'></textarea>
+			<textarea
+				value={newPostStore.content}
+				placeholder='Share your thoughts!'
+				className='form-control'
+				aria-label='With textarea'
+				onChange={handleChange}
+			></textarea>
 			<button
 				className='btn btn-light add-image-icon '
 				type='button'
@@ -12,11 +22,16 @@ const FeedCreatePost: FC = () => {
 			>
 				<i className='bi bi-image text-primary'></i>
 			</button>
-			<button className='btn btn-primary ' type='button' id='button-addon2'>
+			<button
+				onClick={handleSendBtn}
+				className='btn btn-primary '
+				type='button'
+				id='button-addon2'
+			>
 				<i className='bi bi-send-fill'></i>
 			</button>
 		</div>
 	)
-}
+})
 
 export default FeedCreatePost
--- client/src/components/Post/PostCommentForm.tsx
+++ client/src/components/Post/PostCommentForm.tsx
@@ -9,21 +9,7 @@ const PostCommentForm: FC<IPostCommentForm> = () => {
 
 	}
 	return (
-		// <form className='pt-1 pb-1 d-flex align-items-center gap-2'>
-		// 	<input
-		// 		className='bg-light border p-1 mt-1 w-100 post-comment-input form-control'
-		// 		placeholder='Share your thoughts'
-		// 		type='text'
-		// 	/>
-		// 	<button
-		// 		onClick={handleSendBtn}
-		// 		className='btn btn-light border pt-1 pb-1 d-flex align-items-center'
-		// 	>
-		// 		<i className='bi bi-send'></i>
-		// 	</button>
-		// </form>
 		<div className='input-group mt-4'>
-			{/* <span className='input-group-text'>With textarea</span> */}
 			<input
 				placeholder='Share your thoughts!'
 				className='form-control bg-light'
