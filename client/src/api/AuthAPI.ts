1e2a14c20124a02d40ba2c8abfa2673d4a79fa2e
Alexey Skurlatov <Scarlatum@yandex.ru>
2022-08-29 07:21:47 +0800

    [ Refactoring ]: Refactored`AuthAPI` module with some proposals. Updated few interfaces and rearrange imports

--- client/src/api/AuthAPI.ts
+++ client/src/api/AuthAPI.ts
@@ -1,66 +1,117 @@
-import {IUserLogin} from './../models/User/User.types'
-import {IUserToRegister} from './../store/User/User.types'
+import axios, { AxiosResponse } from 'axios'
+
+import {IUserCoreModel, IUserLogin} from './../models/User/User.types';
+import {IUserToRegister} from './../store/User/User.types';
 import userStore from '../store/User/User'
-import axios from 'axios'
+
+import APIMethodDict from './API.json';
+
+// !TODO : Вывести переменную адреса доменав окружение. Явно будет удобнее.
+const PRODUCTION_DOMEN = process.env.prodDomen || 'http://localhost:5000';
+
+namespace Errors {
+	export const enum Auth {
+		CONTRACT = 'Not valid user contract'
+	}
+}
 
 class AuthAPI {
-    static async Login(data: IUserLogin) {
-        const { email, password } = data
-        try {
-            return await axios.post(
-                'http://localhost:5000/api/auth/login',
-                {
-                    email,
-                    password,
-                }
-            )
-        } catch (e: any) {
-            const error = e.response.data.message
-            console.log(error)
-            return error
-        }
-    }
+	static async Login({ email, password }: IUserLogin) {
 
-    static async Logout() {}
+		try {
 
-    static async Register(data: IUserToRegister) {
-        const { email, username, password, link } = data
-        console.log(data)
-        try {
-            return await axios.post(
-                'http://localhost:5000/api/auth/registration',
-                {
-                    email,
-                    username,
-                    password,
-                    link,
-                }
-            )
-        } catch (e: any) {
-            return e.response.data.message
-        }
-    }
+			return await axios.post(
+				`${ PRODUCTION_DOMEN }/${ APIMethodDict.auth.login }`,
+				{
+					email,
+					password,
+				}
+			)
 
-    static async Authenticate() {
-        try {
-            const { data } = await axios.get(
-                'http://localhost:5000/api/auth/authenticate',
-                {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('token')}`,
-                    },
-                }
-            )
-            userStore.setUsername(data.user.username)
-            userStore.setToken(data.token)
-            userStore.setId(data.id)
-            userStore.setAuth(true)
-        } catch (e: any) {
-            console.log(e.response.data.message)
-            localStorage.removeItem('token')
-            localStorage.removeItem('id')
-        }
-    }
+		} catch (e: any) {
+
+			const error = e.response.data.message
+			console.log(error)
+			return error
+
+		}
+
+	}
+
+	static async Logout() {}
+
+	static async Register<T extends object>({ email, username, password, link }: IUserToRegister): Promise<AxiosResponse<T>> {
+
+	  const response = await axios.post<T>(
+			`${ PRODUCTION_DOMEN }/${ APIMethodDict.auth.registration }`,
+			{ email, username, password, link }
+		)
+
+		return response
+
+	}
+
+	static async Authenticate() {
+
+		const Token = localStorage.getItem('token');
+
+		// !TODO : Вывести обработку ошибок куда-то отдельно из тела функции
+		if ( !Token ) throw new Error('Local user token is undefined')
+
+		// !TODO : Выделить контракт между сервером в одельный тип/интерфейс
+		type AuthenticateContract = {
+			user: IUserCoreModel,
+			token: string,
+			// ? По идее ты хранишь ID в двух местах, если судить по интерфейсам. Если ID связанны, то это поле можно игнорировать.  
+			id ?: IUserCoreModel['id'],
+		}
+
+		// !TODO : Вывести ключи в отдельный валидатор.
+		const AuthenticateContractKeys: Array<keyof AuthenticateContract> = ['user', 'token', 'id']
+
+		try {
+
+			const { data } = await axios.get<AuthenticateContract>(
+				// !TODO : Дополнить словарь методов 
+				`${ PRODUCTION_DOMEN }/api/auth/authenticate`,
+				{
+					headers: {
+						Authorization: `Bearer ${ localStorage.getItem('token') }`,
+					},
+				}
+			)
+
+			// ! : Если контракт между сервером поменяется, но ответ будет: Это к чертям положит биндинг пользователя.
+			// ! : Так что, наверное, лучше провалидировать это даже внутри try-catch.
+			// ? : Возможно стоит вывести отдельной функцией, чтобы не засорят метод...
+
+			AuthenticateContractKeys.forEach((key) => {
+
+				if ( data[ key ] === null ) throw Error(`${ Errors.Auth.CONTRACT }: Key ${ key } is undefined in response`);
+
+				switch (key) {
+					case 'user': 
+						userStore.setUsername(data.user.username); break;
+					case 'id':
+						userStore.setId(data.id || data.user.id); break;
+					case 'token':
+						// ? Возможно тут должна быть валидация токена, и уже только потом его присваивание... Но это не мне решать.
+						userStore.setToken(data.token); break
+				}
+
+			})	
+
+			userStore.setAuth(true)
+
+		} catch (e: any) {
+
+			console.debug(e.response.data.message)
+
+			localStorage.removeItem('token');
+			localStorage.removeItem('id');
+
+		}
+	}
 }
 
 export default AuthAPI
--- client/src/pages/RegistrationPage.tsx
+++ client/src/pages/RegistrationPage.tsx
@@ -1,143 +1,160 @@
 import React, { FC, useEffect, useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 import { NavLink } from 'react-router-dom'
-import AuthAPI from '../api/AuthAPI'
-import { Validator } from '../helpers/Validator'
-import userAuthStore from '../store/User/UserAuth'
 import { observer } from 'mobx-react-lite'
+
+// API
+import AuthAPI from '../api/AuthAPI'
+
+// Helpers
+import { Validator } from '../helpers/Validator'
 import { ErrorHandler } from '../helpers/ErrorHandler'
-import Spinner from 'react-bootstrap/Spinner'
+
+// Store
+import userAuthStore from '../store/User/UserAuth'
+
+// Components
+import Form 		from 'react-bootstrap/Form'
+import Button 	from 'react-bootstrap/Button'
+import Card 		from 'react-bootstrap/Card'
+import Spinner 	from 'react-bootstrap/Spinner'
 
 const RegistrationPage: FC = observer(() => {
-    const [isLoading, setLoading] = useState<boolean>(false)
-    const [errors, setErrors] = useState<string[]>([])
-    const [isSuccess, setSuccess] = useState<boolean>(false)
 
-    const handleRegBtn = async (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        setRegistrationVisualProcessingStates()
+	const [isLoading, setLoading] = useState<boolean>(false)
+	const [errors, setErrors] 		= useState<string[]>([])
+	const [isSuccess, setSuccess] = useState<boolean>(false)
 
-        const result: boolean = Validator.ValidateAll(
-            userAuthStore.getUserToValidate()
-        )
-        setErrors(ErrorHandler.GetErrors())
+	const handleRegBtn = async (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault()
+		setRegistrationVisualProcessingStates()
 
-        if (result) {
-            const response = await AuthAPI.Register(
-                userAuthStore.getUserToRegistration()
-            )
-            response.status === 200 ? setSuccess(true) : setErrors([response])
-        }
-        setLoading(false)
-    }
+		const result: boolean = Validator.ValidateAll(
+			userAuthStore.getUserToValidate()
+		)
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value, id } = e.currentTarget
-        userAuthStore.setUserParams(id, value)
-    }
+		setErrors(ErrorHandler.GetErrors())
 
-    const setRegistrationVisualProcessingStates = () => {
-        setSuccess(false)
-        setLoading(true)
-    }
+		if (result) {
 
-    useEffect(() => {}, [errors])
+			const { status, data } = await AuthAPI.Register<{ message: string }>(
+				userAuthStore.getUserToRegistration()
+			)
 
-    return (
-        <div className="container p-3 px-5 d-flex flex-column justify-content-center">
-            <div className="w-100 d-flex justify-content-center mt-1">
-                <Card className="p-5 w-50 d-flex ">
-                    <div className="d-flex flex-column justify-content-center align-items-center gap-0 pb-4 pt-0">
-                        {errors?.map((error: string) => (
-                            <p key={error} className="text-danger m-0 p-0">
-                                {error}
-                            </p>
-                        ))}
-                        {isLoading ? (
-                            <Spinner variant="primary" animation="border" role="status" />
-                        ) : null}
-                        {isSuccess ? (
-                            <p className="text-success m-0 p-0">User successfully created</p>
-                        ) : null}
-                    </div>
+			status === 200
+				? setSuccess(true)
+				: setErrors([data.message])
 
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label className="text-primary">
-                                <strong>Email</strong>
-                            </Form.Label>
-                            <Form.Control
-                                onChange={handleChange}
-                                type="email"
-                                placeholder="Enter email"
-                            />
-                            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
+		}
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label className="text-primary">
-                                <strong>Password</strong>
-                            </Form.Label>
-                            <Form.Control
-                                onChange={handleChange}
-                                type="password"
-                                placeholder="Enter password"
-                            />
-                        </Form.Group>
+		setLoading(false)
 
-                        <Form.Group className="mb-3" controlId="formBasicRepeatPassword">
-                            <Form.Label className="text-primary">
-                                <strong>Repeat password</strong>
-                            </Form.Label>
-                            <Form.Control
-                                onChange={handleChange}
-                                type="password"
-                                placeholder="Repeat password"
-                            />
-                        </Form.Group>
+	}
 
-                        <Form.Group className="mb-3" controlId="formBasicUsername">
-                            <Form.Label className="text-primary">
-                                <strong>Username</strong>
-                            </Form.Label>
-                            <Form.Control
-                                onChange={handleChange}
-                                type="username"
-                                placeholder="Enter username"
-                            />
-                            <Form.Text className="text-muted">
-                Other users will see your username.
-                            </Form.Text>
-                        </Form.Group>
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value, id } = e.currentTarget
+		userAuthStore.setUserParams(id, value)
+	}
 
-                        <Form.Group className="mb-3" controlId="formBasicLink">
-                            <Form.Label className="text-primary">
-                                <strong>Unique page link</strong>
-                            </Form.Label>
-                            <Form.Control
-                                onChange={handleChange}
-                                type="text"
-                                placeholder="Enter unique page link"
-                            />
-                        </Form.Group>
+	const setRegistrationVisualProcessingStates = () => {
+		setSuccess(false)
+		setLoading(true)
+	}
 
-                        <div className="d-flex align-items-center gap-4">
-                            <Button onClick={handleRegBtn} variant="primary" type="submit">
-                Register
-                            </Button>
-                            <NavLink to="/" className="text-decoration-underline pointer">
-                Already registered? Back to login page
-                            </NavLink>
-                        </div>
-                    </Form>
-                </Card>
-            </div>
-        </div>
-    )
+	useEffect(() => { }, [errors])
+
+	return (
+		<div className="container p-3 px-5 d-flex flex-column justify-content-center">
+			<div className="w-100 d-flex justify-content-center mt-1">
+				<Card className="p-5 w-50 d-flex">
+					<div className="d-flex flex-column justify-content-center align-items-center gap-0 pb-4 pt-0">
+						{errors?.map((error: string) => (
+							<p key={error} className="text-danger m-0 p-0">
+								{error}
+							</p>
+						))}
+						{isLoading ? (
+							<Spinner variant="primary" animation="border" role="status" />
+						) : null}
+						{isSuccess ? (
+							<p className="text-success m-0 p-0">User successfully created</p>
+						) : null}
+					</div>
+
+					<Form>
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label className="text-primary">
+								<strong>Email</strong>
+							</Form.Label>
+							<Form.Control
+								onChange={handleChange}
+								type="email"
+								placeholder="Enter email"
+							/>
+							<Form.Text className="text-muted">
+								We'll never share your email with anyone else.
+							</Form.Text>
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label className="text-primary">
+								<strong>Password</strong>
+							</Form.Label>
+							<Form.Control
+								onChange={handleChange}
+								type="password"
+								placeholder="Enter password"
+							/>
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicRepeatPassword">
+							<Form.Label className="text-primary">
+								<strong>Repeat password</strong>
+							</Form.Label>
+							<Form.Control
+								onChange={handleChange}
+								type="password"
+								placeholder="Repeat password"
+							/>
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicUsername">
+							<Form.Label className="text-primary">
+								<strong>Username</strong>
+							</Form.Label>
+							<Form.Control
+								onChange={handleChange}
+								type="username"
+								placeholder="Enter username"
+							/>
+							<Form.Text className="text-muted">
+								Other users will see your username.
+							</Form.Text>
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicLink">
+							<Form.Label className="text-primary">
+								<strong>Unique page link</strong>
+							</Form.Label>
+							<Form.Control
+								onChange={handleChange}
+								type="text"
+								placeholder="Enter unique page link"
+							/>
+						</Form.Group>
+
+						<div className="d-flex align-items-center gap-4">
+							<Button onClick={handleRegBtn} variant="primary" type="submit">
+								Register
+							</Button>
+							<NavLink to="/" className="text-decoration-underline pointer">
+								Already registered? Back to login page
+							</NavLink>
+						</div>
+					</Form>
+				</Card>
+			</div>
+		</div>
+	)
 })
 
 export default RegistrationPage
